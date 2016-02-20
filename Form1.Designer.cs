ec728b3f62a04c014196393834a667ce7a7661ad
spaghettisource <natureelement@gmail.com>
2016-02-20 19:06:57 +0200

    initial version

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WebPresenter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectedImage = new System.Windows.Forms.PictureBox();
+            this.btnSendImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectedImage
+            // 
+            this.selectedImage.Image = ((System.Drawing.Image)(resources.GetObject("selectedImage.Image")));
+            this.selectedImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("selectedImage.InitialImage")));
+            this.selectedImage.Location = new System.Drawing.Point(12, 12);
+            this.selectedImage.Name = "selectedImage";
+            this.selectedImage.Size = new System.Drawing.Size(722, 384);
+            this.selectedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.selectedImage.TabIndex = 0;
+            this.selectedImage.TabStop = false;
+            this.selectedImage.WaitOnLoad = true;
+            // 
+            // btnSendImage
+            // 
+            this.btnSendImage.Location = new System.Drawing.Point(331, 419);
+            this.btnSendImage.Name = "btnSendImage";
+            this.btnSendImage.Size = new System.Drawing.Size(75, 23);
+            this.btnSendImage.TabIndex = 1;
+            this.btnSendImage.Text = "Send Image";
+            this.btnSendImage.UseVisualStyleBackColor = true;
+            this.btnSendImage.Click += new System.EventHandler(this.btnSendImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 454);
+            this.Controls.Add(this.btnSendImage);
+            this.Controls.Add(this.selectedImage);
+            this.Name = "Form1";
+            this.Text = "Web presenter";
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox selectedImage;
+        private System.Windows.Forms.Button btnSendImage;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebPresenter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendImage_Click(object sender, EventArgs e)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            selectedImage.Image.Save(memoryStream, ImageFormat.Jpeg);
+            byte[] buffer = memoryStream.ToArray();
+            
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+
+            listener.Prefixes.Add("http://localhost:8080/");
+
+            listener.Start();
+            Console.WriteLine("Listening...");
+            // Note: The GetContext method blocks while waiting for a request. 
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest listenerrequest = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse listenerresponse = context.Response;
+            // Construct a response.           
+            listenerresponse.ContentLength64 = buffer.Length;
+            // Get a response stream and write the response to it.
+            System.IO.Stream output = listenerresponse.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            listener.Stop();
+        }
+
+        private void sendStream() { }
+
+        
+    }
+}
