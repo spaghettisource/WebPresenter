61945692172bae261107b79976d7b8460f9331f9
spaghettisource <natureelement@gmail.com>
2016-02-27 17:34:47 +0200

    application logic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,16 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.selectedImage = new System.Windows.Forms.PictureBox();
             this.btnSendImage = new System.Windows.Forms.Button();
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dialogOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnGetAddress = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).BeginInit();
+            this.menuMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // selectedImage
             // 
-            this.selectedImage.Image = ((System.Drawing.Image)(resources.GetObject("selectedImage.Image")));
             this.selectedImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("selectedImage.InitialImage")));
-            this.selectedImage.Location = new System.Drawing.Point(12, 12);
+            this.selectedImage.Location = new System.Drawing.Point(12, 27);
             this.selectedImage.Name = "selectedImage";
-            this.selectedImage.Size = new System.Drawing.Size(722, 384);
+            this.selectedImage.Size = new System.Drawing.Size(722, 386);
             this.selectedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.selectedImage.TabIndex = 0;
             this.selectedImage.TabStop = false;
@@ -48,7 +55,7 @@
             // 
             // btnSendImage
             // 
-            this.btnSendImage.Location = new System.Drawing.Point(331, 419);
+            this.btnSendImage.Location = new System.Drawing.Point(659, 419);
             this.btnSendImage.Name = "btnSendImage";
             this.btnSendImage.Size = new System.Drawing.Size(75, 23);
             this.btnSendImage.TabIndex = 1;
@@ -56,17 +63,79 @@
             this.btnSendImage.UseVisualStyleBackColor = true;
             this.btnSendImage.Click += new System.EventHandler(this.btnSendImage_Click);
             // 
+            // menuMain
+            // 
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.helpToolStripMenuItem});
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Size = new System.Drawing.Size(746, 24);
+            this.menuMain.TabIndex = 2;
+            this.menuMain.Text = "menuMain";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // dialogOpenFile
+            // 
+            this.dialogOpenFile.Filter = " \"Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfi" +
+    "f; *.png\";";
+            this.dialogOpenFile.FileOk += new System.ComponentModel.CancelEventHandler(this.dialogOpenFile_FileOk);
+            // 
+            // btnGetAddress
+            // 
+            this.btnGetAddress.Location = new System.Drawing.Point(578, 419);
+            this.btnGetAddress.Name = "btnGetAddress";
+            this.btnGetAddress.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAddress.TabIndex = 3;
+            this.btnGetAddress.Text = "Get Address";
+            this.btnGetAddress.UseVisualStyleBackColor = true;
+            this.btnGetAddress.Click += new System.EventHandler(this.btnGetAddress_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(746, 454);
+            this.Controls.Add(this.btnGetAddress);
             this.Controls.Add(this.btnSendImage);
             this.Controls.Add(this.selectedImage);
+            this.Controls.Add(this.menuMain);
             this.Name = "Form1";
             this.Text = "Web presenter";
             ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).EndInit();
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +143,13 @@
 
         private System.Windows.Forms.PictureBox selectedImage;
         private System.Windows.Forms.Button btnSendImage;
+        private System.Windows.Forms.MenuStrip menuMain;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog dialogOpenFile;
+        private System.Windows.Forms.Button btnGetAddress;
     }
 }
 
--- MainWindow.cs
+++ MainWindow.cs
@@ -7,6 +7,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,10 +23,14 @@ namespace WebPresenter
 
         private void btnSendImage_Click(object sender, EventArgs e)
         {
+            sendStream();
+        }
+
+        private void sendStream() {
             MemoryStream memoryStream = new MemoryStream();
             selectedImage.Image.Save(memoryStream, ImageFormat.Jpeg);
             byte[] buffer = memoryStream.ToArray();
-            
+
             // Create a listener.
             HttpListener listener = new HttpListener();
 
@@ -42,14 +47,69 @@ namespace WebPresenter
             listenerresponse.ContentLength64 = buffer.Length;
             // Get a response stream and write the response to it.
             System.IO.Stream output = listenerresponse.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            // You must close the output stream.
-            output.Close();
-            listener.Stop();
+            try
+            {
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error sending the file");
+            }
+            finally
+            {
+                output.Close();
+                listener.Stop();
+            }
         }
 
-        private void sendStream() { }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dialogOpenFile.ShowDialog();            
+        }
 
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A simple program to send images without uploading them");
+        }
+
+        private void dialogOpenFile_FileOk(object sender, CancelEventArgs e)
+        {            
+            string filetype = System.Web.MimeMapping.GetMimeMapping(dialogOpenFile.FileName);
+            if (filetype.StartsWith("image"))
+            {
+            try
+            {
+                selectedImage.LoadAsync(dialogOpenFile.FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error loading the file");
+            }
+            }
+            else
+            {
+                MessageBox.Show("You are trying to a file that is not an image");
+            }
+        }
+
+        private void btnGetAddress_Click(object sender, EventArgs e)
+        {
+            //string localIP;
+            //using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            //{
+            //    socket.Connect("10.0.2.4", 65530);
+            //    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+            //    localIP = endPoint.Address.ToString();
+            //}
+
+            MessageBox.Show("255.255.255.0", "IP Address");            
+        }
+
+        private void GetIPAddress()
+        {
+
+        }
         
     }
 }
